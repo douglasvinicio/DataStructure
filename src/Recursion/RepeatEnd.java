3c78c374e94e929160fadbe6a21ce0ece6951f71
douglasvinicio <douglas.vinicio@hotmail.com>
2021-02-09 14:24:18 -0300

    Practice

---
+++ src/Recursion/RepeatEnd.java
@@ -0,0 +1,25 @@
+package Recursion;
+//Given a string and an int n, return a string made of n repetitions of the last n characters of the string.
+// You may assume that n is between 0 and the length of the string, inclusive.
+//
+// repeatEnd("Hello", 3) → "llollollo"
+// repeatEnd("Hello", 2) → "lolo"
+// repeatEnd("Hello", 1) → "o"
+
+public class RepeatEnd {
+    public static void main(String[] args) {
+        System.out.println(repeatEnd("Hello", 3));
+        System.out.println(repeatEnd("Hello", 2));
+    }
+
+    public static String repeatEnd(String str, int n){
+
+        StringBuffer result = new StringBuffer();
+        String end = str.substring(str.length() - n);
+
+        for (int i = 0; i < n; i++)
+            result.append(end);
+
+        return result.toString();
+        }
+    }
---
+++ src/Recursion/SplitArray.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class SplitArray {
+    public static void main(String[] args) {
+        int[] nums = {5,2,3};
+        System.out.println(splitArray(nums));
+    }
+
+    public static boolean splitArray(int[] nums){
+        return splitArrayHelper(0,nums,0,0);
+    }
+
+    public static boolean splitArrayHelper(int start, int[] nums, int group1, int group2){
+        if (start >= nums.length)
+            return group1 == group2;
+        if(splitArrayHelper(start + 1, nums, group1 + nums[start], group2))
+            return true;
+        if (splitArrayHelper(start + 1, nums, group1, group2 + nums[start]))
+            return true;
+        return false;
+    }
+}
